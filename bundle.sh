3e6c44112b6b5d0b7215704ec88f4678153fb902
Sibi Prabakaran <sibi@psibi.in>
2016-11-13 19:18:06 +0530

    Add bundling script

---
+++ bundle.sh
@@ -0,0 +1,19 @@
+# Script for bundling a firefox and chrome zip plugin
+
+# Chrome bundling
+rm -rf ./chrome
+mkdir chrome
+cp manifest.json stackage.js ./chrome
+cp -r icons ./chrome
+cd chrome
+zip -r ../chrome.zip *
+cd ..
+
+#Firefox bundling
+# rm -rf ./firefox
+# mkdir firefox
+# cp manifest.json stackage.js ./firefox
+# cp -r icons ./firefox
+# cd firefox
+# zip -r ../firefox.zip *
+# cd ..
