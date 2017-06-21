20b8361dcb34f948022539f7ee49d0649e3378cd
Sibi Prabakaran <sprabakaran@xebia.com>
2017-06-21 13:13:45 +0530

    Make bundle working

--- bundle.sh
+++ bundle.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Script for bundling a firefox and chrome zip plugin
 
 # Chrome bundling
@@ -5,15 +6,16 @@ rm -rf ./chrome
 mkdir chrome
 cp manifest.json stackage.js ./chrome
 cp -r icons ./chrome
-cd chrome
-zip -r ../chrome.zip *
-cd ..
+# cd chrome
+# zip -r ../chrome.zip *
+# cd ..
 
 #Firefox bundling
-# rm -rf ./firefox
-# mkdir firefox
-# cp manifest.json stackage.js ./firefox
-# cp -r icons ./firefox
-# cd firefox
-# zip -r ../firefox.zip *
-# cd ..
+rm -rf ./firefox
+mkdir firefox
+cp manifest.json stackage.js ./firefox
+cp -r icons ./firefox
+cp -r settings ./firefox
+cd firefox
+zip -r ../firefox.zip *
+cd ..
