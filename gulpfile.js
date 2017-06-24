72277c7a1a9dca2191d32f209190abd6570e5d63
Sibi Prabakaran <sibi@psibi.in>
2017-06-24 12:34:26 +0530

    change rules

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,17 @@ const gulp = require('gulp');
 const preprocess = require('gulp-preprocess');
 const gulpCopy = require('gulp-copy');
 
-const sourceFiles = ['manifest.json', 'stackage.js', 'icons/*', 'settings/*']
+const sourceFiles = ['manifest.json', 'icons/*', 'settings/options.html']
 
 gulp.task('chrome', function() {
   gulp.src(sourceFiles)
       .pipe(gulpCopy('./chrome/'))
-  
-  gulp.src('./chrome/stackage.js')
+
+  gulp.src(['./stackage.js'])
       .pipe(preprocess({context: {BROWSER_ENV: 'CHROME'}}))
       .pipe(gulp.dest('./chrome/'))
+
+  gulp.src(['./settings/options.js'])
+      .pipe(preprocess({context: {BROWSER_ENV: 'CHROME'}}))
+      .pipe(gulp.dest('./chrome/settings/'))
 });
